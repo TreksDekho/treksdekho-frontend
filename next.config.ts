fdf677d940e11636287e555cfe17ec523c58339d
raaahuuulll <rahulhindocha05@gmail.com>
2026-05-20 23:06:28 +0530

    release 0.1

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/treksdekho-frontend",
+  assetPrefix: "/treksdekho-frontend/",
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
